cddcbafb5f2fd0bb2e18e732448a8b06f554453d
kirkmartin76248-oss <kirk.martin.76248@gmail.com>
2026-09-17 20:35:43 -0400

    Integrate hub BLE commissioning mode

--- hub/WaterSensorHub/WaterSensorHub.ino
+++ hub/WaterSensorHub/WaterSensorHub.ino
@@ -2,15 +2,38 @@
 #include "HubStorage.h"
 #include "LoRaHub.h"
 #include "Cloud.h"
+#include "BleConfig.h"
 
 HubConfig cfg;
 HubStorage storage;
 LoRaHub lora;
 Cloud cloud;
+HubBleConfig ble;
+
+static bool enterBleIfRequested() {
+  // GPIO9 is the XIAO ESP32-C6 BOOT button. Do not hold it during reset;
+  // GPIO9 is a boot-strapping pin. Press it after the application starts.
+  pinMode(9, INPUT);
+  uint32_t start = millis();
+  while (millis() - start < 2000UL) {
+    if (digitalRead(9) == LOW) {
+      ble.begin(cfg, storage);
+      while (ble.active()) {
+        ble.loop(cfg, storage);
+        delay(10);
+      }
+      ESP.restart();
+      return true;
+    }
+    delay(10);
+  }
+  return false;
+}
 
 void setup() {
   Serial.begin(115200);
   delay(300);
+
   storage.begin();
   storage.loadConfig(cfg);
 
@@ -19,6 +42,8 @@ void setup() {
   Serial.println("----------------------");
   Serial.printf("Hub ID: %s\n", cfg.hubId.c_str());
   Serial.printf("Poll interval: %lu s\n", (unsigned long)cfg.pollIntervalSec);
+
+  if (enterBleIfRequested()) return;
 
   if (!lora.begin(cfg)) {
     Serial.println("LoRa init FAILED");
@@ -30,6 +55,7 @@ void setup() {
 void loop() {
   for (uint8_t i = 0; i < cfg.nodeCount; ++i) {
     if (!cfg.nodes[i].enabled) continue;
+
     Telemetry t;
     bool ok = lora.pollNode(cfg.nodes[i], t, cfg);
     if (ok) {
@@ -42,6 +68,7 @@ void loop() {
     }
     delay(50);
   }
+
   cloud.refreshConfig(cfg, storage);
   delay(100);
 }
